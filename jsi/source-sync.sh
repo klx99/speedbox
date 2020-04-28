2ab040185cf0d60c8fe329b288e7da3629d58b91
mengxk <xiaokun.meng@qcast.cn>
2020-04-28 09:09:02 +0800

    update jsi scripts.

--- jsi/source-sync.sh
+++ jsi/source-sync.sh
@@ -8,9 +8,10 @@ PROJECT_DIR=$(dirname $(dirname "$SCRIIPTS_DIR"));
 
 export PATH+=":$PROJECT_DIR/depot_tools";
 
-echo change dir to $PROJECT_DIR;
+echo "change dir to $PROJECT_DIR";
 cd $PROJECT_DIR;
 
+echo "checking depot_tools";
 set +o errexit
 GCLIENT_PATH=$(which gclient);
 set -o errexit
@@ -19,6 +20,7 @@ if [[ "$GCLIENT_PATH" == "" ]]; then
     git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git;
 fi
 
+echo "generating .gclient";
 echo 'solutions = [ {'                                    >  .gclient;
 echo '  "name": "v8",'                                    >> .gclient;
 echo '  "url": "https://github.com/m15998568620/v8.git",' >> .gclient;
@@ -26,6 +28,7 @@ echo '  "deps_file": "DEPS",'                             >> .gclient;
 echo '  "managed": False,'                                >> .gclient;
 echo '  "custom_deps": {},'                               >> .gclient;
 echo '}, ]'                                               >> .gclient;
-echo 'target_os = ['android']'                            >> .gclient;
+echo 'target_os = ["android"]'                            >> .gclient;
 
+echo "syncing";
 gclient sync;
